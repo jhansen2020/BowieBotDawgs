bf961a81c70ecec4e5c566eb614ff9876ce422e6
Nickbastos <nickbastos4@gmail.com>
2017-10-13 17:13:13 -0500

    Changed the names of the variables

--- TeamCode/src/main/java/TestTeleOpMode.java
+++ TeamCode/src/main/java/TestTeleOpMode.java
@@ -13,10 +13,11 @@ public class TestTeleOpMode extends OpMode{
     //Creating variables
 
     private ElapsedTime runtime = new ElapsedTime();
-    private DcMotor Leftfront, Leftback, Rightfront, Rightback, Lift;
-    private Servo Leftclamp, Rightclamp;
 
-    double Leftstick, Rightstick, Leftpower, Rightpower, Liftpower, Clamppower, Leftservopower, Rightservopower;
+    private DcMotor leftFrontMotor, leftBackMotor, rightFrontMotor, rightBackMotor,liftMotor;
+    private Servo leftClampServo, rightClampServo;
+
+    double leftJoyStick, rightJoyStick, leftMotorPower, rightMotorPower, liftMotorPower, clampMotorPower, leftServoPower, rightServoPower;
 
     final private static double JOYSTICK_DEADBAND = 0.1;
 
@@ -25,24 +26,24 @@ public class TestTeleOpMode extends OpMode{
 
         //Assigning variables
 
-        Leftfront = hardwareMap.dcMotor.get("Leftfront");
-        Leftback = hardwareMap.dcMotor.get("Leftback");
-        Rightfront = hardwareMap.dcMotor.get("Rightfront");
-        Rightback = hardwareMap.dcMotor.get("Rightback");
-        Lift = hardwareMap.dcMotor.get("Lift");
-        Leftclamp = hardwareMap.servo.get("LeftClamp");
-        Rightclamp = hardwareMap.servo.get("RightClamp");
+        leftFrontMotor = hardwareMap.dcMotor.get("Leftfront");
+        leftBackMotor = hardwareMap.dcMotor.get("Leftback");
+        rightFrontMotor = hardwareMap.dcMotor.get("Rightfront");
+        rightBackMotor = hardwareMap.dcMotor.get("Rightback");
+        liftMotor = hardwareMap.dcMotor.get("Lift");
+        leftClampServo = hardwareMap.servo.get("LeftClamp");
+        rightClampServo = hardwareMap.servo.get("RightClamp");
 
         //Assigning directions of motors
 
-        Leftfront.setDirection(DcMotor.Direction.REVERSE);
-        Leftback.setDirection(DcMotor.Direction.REVERSE);
-        Rightfront.setDirection(DcMotor.Direction.FORWARD);
-        Rightback.setDirection(DcMotor.Direction.FORWARD);
+        leftFrontMotor.setDirection(DcMotor.Direction.REVERSE);
+        leftBackMotor.setDirection(DcMotor.Direction.REVERSE);
+        rightFrontMotor.setDirection(DcMotor.Direction.FORWARD);
+        rightBackMotor.setDirection(DcMotor.Direction.FORWARD);
 
-        //Preassigning Clamppower
+        //Preassigning clampMotorPower
 
-        Clamppower = -1.0;
+        clampMotorPower = -1.0;
     }
 
     //Code that resets the elapsed time once the driver hits play
@@ -56,46 +57,46 @@ public class TestTeleOpMode extends OpMode{
 
         //Assigning gamepad values
 
-        Leftstick = -gamepad1.left_stick_y;
-        Rightstick = gamepad1.right_stick_x;
+        leftJoyStick = -gamepad1.left_stick_y;
+        rightJoyStick = gamepad1.right_stick_x;
 
-        Clamppower += (gamepad2.right_trigger > 0.5 && gamepad2.left_trigger < 0.3) ? 0.05 :
+        clampMotorPower += (gamepad2.right_trigger > 0.5 && gamepad2.left_trigger < 0.3) ? 0.05 :
                 (gamepad2.right_trigger < 0.3 && gamepad2.left_trigger > 0.5) ? - 0.05 : 0;
 
-        Liftpower = gamepad2.dpad_up && !gamepad2.dpad_down ? 0.2 :
+        liftMotorPower = gamepad2.dpad_up && !gamepad2.dpad_down ? 0.2 :
                     !gamepad1.dpad_up && gamepad2.dpad_down ? -0.2 : 0.0;
 
         //Testing JOYSTICK_DEADBAND
 
-        if (Math.abs(Leftstick) < JOYSTICK_DEADBAND) Leftstick = 0;
-        if (Math.abs(Rightstick) < JOYSTICK_DEADBAND) Rightstick = 0;
+        if (Math.abs(leftJoyStick) < JOYSTICK_DEADBAND) leftJoyStick = 0;
+        if (Math.abs(rightJoyStick) < JOYSTICK_DEADBAND) rightJoyStick = 0;
 
         //Assiging POV drive values
 
-        Leftpower = Range.clip(Leftstick + Rightstick, -1.0, 1.0);
-        Rightpower = Range.clip(Leftstick - Rightstick, -1.0, 1.0);
+        leftMotorPower = Range.clip(leftJoyStick + rightJoyStick, -1.0, 1.0);
+        rightMotorPower = Range.clip(leftJoyStick - rightJoyStick, -1.0, 1.0);
 
-        //Assigning power to each servo and clipping Clamppower
+        //Assigning power to each servo and clipping clampMotorPower
 
-        Clamppower = Range.clip(Clamppower, -1.0, 1.0);
+        clampMotorPower = Range.clip(clampMotorPower, -1.0, 1.0);
 
-        Leftservopower = -Clamppower;   //might need to reverse the negative sign
-        Rightservopower = Clamppower;
+        leftServoPower = -clampMotorPower;   //might need to reverse the negative sign
+        rightServoPower = clampMotorPower;
 
         //Applying power to motors and servos
 
-        Leftfront.setPower(Leftpower);
-        Leftback.setPower(Leftpower);
-        Rightfront.setPower(Rightpower);
-        Rightback.setPower(Rightpower);
-        Lift.setPower(Liftpower);
+        leftFrontMotor.setPower(leftMotorPower);
+        leftBackMotor.setPower(leftMotorPower);
+        rightFrontMotor.setPower(rightMotorPower);
+        rightBackMotor.setPower(rightMotorPower);
+        liftMotor.setPower(liftMotorPower);
 
-        Leftclamp.setPosition(Leftservopower);
-        Rightclamp.setPosition(Rightservopower);
+        leftClampServo.setPosition(leftServoPower);
+        rightClampServo.setPosition(rightServoPower);
 
         // Show the elapsed game time and wheel power.
         telemetry.addData("Status", "Run Time: " + runtime.toString());
-        telemetry.addData("Motors", "left (%.2f), right (%.2f)", Leftpower, Rightpower);
-        telemetry.addData("Servo:", Clamppower);
+        telemetry.addData("Motors", "left (%.2f), right (%.2f)", leftMotorPower, rightMotorPower);
+        telemetry.addData("Servo:", clampMotorPower);
     }
 }
