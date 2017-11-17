c3ee47d100afbcc8ff94c9d767defa0973e38633
Nickbastos <nickbastos4@gmail.com>
2017-11-17 11:41:05 -0600

    Test

--- TeamCode/src/main/java/MainTeleOpMode.java
+++ TeamCode/src/main/java/MainTeleOpMode.java
@@ -23,9 +23,20 @@ public class MainTeleOpMode extends OpMode{
     double leftJoyStick, rightJoyStick, leftMotorPower, rightMotorPower, liftMotorPower, clampMotorPower, leftServoPower, rightServoPower;
 
     final private static double JOYSTICK_DEADBAND = 0.1;
+    //Encoder Ticks Variables
+    static final double     COUNTS_PER_MOTOR_REV    = 1440 ;    // eg: TETRIX Motor Encoder
+    static final double     DRIVE_GEAR_REDUCTION    = 2.0 / 3 ;     // This is < 1.0 if geared UP
+    static final double     WHEEL_DIAMETER_INCHES   = 4.0 ;     // For figuring circumference
+    static final double     COUNTS_PER_INCH         = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
+            (WHEEL_DIAMETER_INCHES * 3.1415);
+    static final double     DRIVE_SPEED             = 0.6;
+    static final double     TURN_SPEED              = 0.5;
 
-    double motorMin = 0.0;
-    double motorMax = 0.0;
+    //Lift Limit varibles
+    double liftUpdatedTicks = liftMotor.getCurrentPosition();
+    double liftTotalTicks =  COUNTS_PER_INCH * 27.5;
+
+
 
     @Override
     public void init() {
@@ -50,8 +61,6 @@ public class MainTeleOpMode extends OpMode{
 
         rightServoPower = 0.75;
         leftServoPower = -0.75;
-
-
     }
 
     //Code that resets the elapsed time once the driver hits play
@@ -71,19 +80,10 @@ public class MainTeleOpMode extends OpMode{
         leftJoyStick = -gamepad1.left_stick_y;
         rightJoyStick = gamepad1.right_stick_x;
 
-        if (gamepad1.a && !gamepad1.y){
-            motorMin = -0.4;
-            motorMax = 0.3;
-            
-        }else if (gamepad1.y && !gamepad1.a){
-            motorMin = -0.15;
-            motorMax = 0.15;
-        }
-
 
         if (gamepad2.right_trigger > 0.5 && gamepad2.left_trigger < 0.3){
             rightServoPower = -1;
-            leftServoPower = .85;
+            leftServoPower = .9;
         } else if (gamepad2.right_trigger < 0.3 && gamepad2.left_trigger > 0.5){
             rightServoPower = .75;
             leftServoPower = -.8;
@@ -93,15 +93,21 @@ public class MainTeleOpMode extends OpMode{
         liftMotorPower = gamepad2.dpad_up && !gamepad2.dpad_down ? -1 :
                 !gamepad1.dpad_up && gamepad2.dpad_down ? 1 : 0.0;
 
-        //Testing JOYSTICK_DEADBAND
+
+        if (gamepad2.dpad_up && !gamepad2.dpad_down && liftUpdatedTicks < liftTotalTicks && liftUpdatedTicks > 0) {
+            liftMotorPower = -1;
+        } else if (!gamepad2.dpad_up && gamepad2.dpad_down && liftUpdatedTicks < liftTotalTicks && liftUpdatedTicks > 0) {
+            liftMotorPower = 1;
+        }
+
 
         if (Math.abs(leftJoyStick) < JOYSTICK_DEADBAND) leftJoyStick = 0;
         if (Math.abs(rightJoyStick) < JOYSTICK_DEADBAND) rightJoyStick = 0;
 
         //Assiging POV drive values
 
-        leftMotorPower = Range.clip(leftJoyStick + rightJoyStick, motorMin, motorMax);
-        rightMotorPower = Range.clip(leftJoyStick - rightJoyStick, motorMin, motorMax);
+        leftMotorPower = Range.clip(leftJoyStick + rightJoyStick, -0.4  , 0.3);
+        rightMotorPower = Range.clip(leftJoyStick - rightJoyStick, -0.4, 0.3);
 
         //Assigning power to each servo and clipping clampMotorPower
 
@@ -125,7 +131,4 @@ public class MainTeleOpMode extends OpMode{
         telemetry.addData("RightServoPower", "power: (%.2f)", rightServoPower);
 
     }
-
-
-    //use stop function to go back to us
 }
