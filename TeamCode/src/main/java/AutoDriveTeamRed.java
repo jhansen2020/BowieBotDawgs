c8c37d12228ef28f7e2c2b910d10f7ae97847276
Daniel <daniel.yllana.santiago@gmail.com>
2018-01-25 16:17:18 -0600

    Autonomus for team blue and red

--- TeamCode/src/main/java/AutoDriveTeamRed.java
+++ TeamCode/src/main/java/AutoDriveTeamRed.java
@@ -101,58 +101,6 @@ public class AutoDriveTeamRed extends LinearOpMode {
          * Initialize the drive system variables.
          * The init() method of the hardware class does all the work here
          */
-
-
-        robot.init(hardwareMap);
-
-
-
-        // Turn on the LED light for better accuracy of the scan
-        robot.colorSensor.enableLed(true);
-
-        // Send telemetry message to signify robot waiting;
-        telemetry.addData("Status", "Resetting Encoders");
-        telemetry.update();
-
-        robot.leftFrontMotor = hardwareMap.dcMotor.get("Leftfront");
-        robot.leftBackMotor = hardwareMap.dcMotor.get("Leftback");
-        robot.rightFrontMotor = hardwareMap.dcMotor.get("Rightfront");
-        robot.rightBackMotor = hardwareMap.dcMotor.get("Rightback");
-        robot.liftMotor = hardwareMap.dcMotor.get("Lift");
-        robot.leftClampServo = hardwareMap.servo.get("LeftClamp");
-        robot.rightClampServo = hardwareMap.servo.get("RightClamp");
-
-        // Send telemetry message to indicate successful Encoder reset
-        telemetry.addData("Path0",  "Starting at %7d :%7d",
-                          robot.leftFrontMotor.getCurrentPosition(),
-                          robot.rightFrontMotor.getCurrentPosition(),
-                          robot.leftBackMotor.getCurrentPosition(),
-                          robot.rightBackMotor.getCurrentPosition());
-        telemetry.update();
-
-
-        // Wait for the game to start (driver presses PLAY)
-        waitForStart();
-        telemetry.addData("Blue", robot.colorSensor.blue());
-        telemetry.addData("Red", robot.colorSensor.red());
-
-
-        armDown(2.5);//THIS WILL MOVE THE ARM WITH THE COLOR SENSOR DOWN
-        jewel(2.5);//THIS WILL SCAN THE COLOR, DECIDE IN WHAT DIRECTION TO TURN, AND TURN
-
-
-
-
-
-
-        sleep(1000);     // pause for servos to move
-
-        telemetry.addData("Path", "Complete");
-        telemetry.update();
-
-
-
-
         //Vuforia
 
 
@@ -206,54 +154,106 @@ public class AutoDriveTeamRed extends LinearOpMode {
 
         relicTrackables.activate();
 
-        while (opModeIsActive()) {
-
-            /**
-             * See if any of the instances of {@link relicTemplate} are currently visible.
-             * {@link RelicRecoveryVuMark} is an enum which can have the following values:
-             * UNKNOWN, LEFT, CENTER, and RIGHT. When a VuMark is visible, something other than
-             * UNKNOWN will be returned by {@link RelicRecoveryVuMark#from(VuforiaTrackable)}.
-             */
-            vuMark = RelicRecoveryVuMark.from(relicTemplate);
 
 
+        /**
+         * See if any of the instances of {@link relicTemplate} are currently visible.
+         * {@link RelicRecoveryVuMark} is an enum which can have the following values:
+         * UNKNOWN, LEFT, CENTER, and RIGHT. When a VuMark is visible, something other than
+         * UNKNOWN will be returned by {@link RelicRecoveryVuMark#from(VuforiaTrackable)}.
+         */
+        vuMark = RelicRecoveryVuMark.from(relicTemplate);
 
-            if (vuMark != RelicRecoveryVuMark.UNKNOWN) {
+
+
+        if (vuMark != RelicRecoveryVuMark.UNKNOWN) {
 
                 /* Found an instance of the template. In the actual game, you will probably
                  * loop until this condition occurs, then move on to act accordingly depending
                  * on which VuMark was visible. */
-                telemetry.addData("VuMark", "%s visible", vuMark);
+            telemetry.addData("VuMark", "%s visible", vuMark);
 
                 /* For fun, we also exhibit the navigational pose. In the Relic Recovery game,
                  * it is perhaps unlikely that you will actually need to act on this pose information, but
                  * we illustrate it nevertheless, for completeness. */
-                OpenGLMatrix pose = ((VuforiaTrackableDefaultListener)relicTemplate.getListener()).getPose();
-                telemetry.addData("Pose", format(pose));
+            OpenGLMatrix pose = ((VuforiaTrackableDefaultListener)relicTemplate.getListener()).getPose();
+            telemetry.addData("Pose", format(pose));
 
                 /* We further illustrate how to decompose the pose into useful rotational and
                  * translational components */
-                if (pose != null) {
-                    VectorF trans = pose.getTranslation();
-                    Orientation rot = Orientation.getOrientation(pose, AxesReference.EXTRINSIC, AxesOrder.XYZ, AngleUnit.DEGREES);
+            if (pose != null) {
+                VectorF trans = pose.getTranslation();
+                Orientation rot = Orientation.getOrientation(pose, AxesReference.EXTRINSIC, AxesOrder.XYZ, AngleUnit.DEGREES);
 
-                    // Extract the X, Y, and Z components of the offset of the target relative to the robot
-                    double tX = trans.get(0);
-                    double tY = trans.get(1);
-                    double tZ = trans.get(2);
+                // Extract the X, Y, and Z components of the offset of the target relative to the robot
+                double tX = trans.get(0);
+                double tY = trans.get(1);
+                double tZ = trans.get(2);
 
-                    // Extract the rotational components of the target relative to the robot
-                    double rX = rot.firstAngle;
-                    double rY = rot.secondAngle;
-                    double rZ = rot.thirdAngle;
-                }
+                // Extract the rotational components of the target relative to the robot
+                double rX = rot.firstAngle;
+                double rY = rot.secondAngle;
+                double rZ = rot.thirdAngle;
             }
-            else {
-                telemetry.addData("VuMark", "not visible");
-            }
-
-            telemetry.update();
         }
+        else {
+            telemetry.addData("VuMark", "not visible");
+        }
+
+        telemetry.update();
+
+
+        robot.init(hardwareMap);
+
+
+
+        // Turn on the LED light for better accuracy of the scan
+        robot.colorSensor.enableLed(true);
+
+        // Send telemetry message to signify robot waiting;
+        telemetry.addData("Status", "Resetting Encoders");
+        telemetry.update();
+
+        robot.leftFrontMotor = hardwareMap.dcMotor.get("Leftfront");
+        robot.leftBackMotor = hardwareMap.dcMotor.get("Leftback");
+        robot.rightFrontMotor = hardwareMap.dcMotor.get("Rightfront");
+        robot.rightBackMotor = hardwareMap.dcMotor.get("Rightback");
+        robot.liftMotor = hardwareMap.dcMotor.get("Lift");
+        robot.leftClampServo = hardwareMap.servo.get("LeftClamp");
+        robot.rightClampServo = hardwareMap.servo.get("RightClamp");
+
+        // Send telemetry message to indicate successful Encoder reset
+        telemetry.addData("Path0",  "Starting at %7d :%7d",
+                          robot.leftFrontMotor.getCurrentPosition(),
+                          robot.rightFrontMotor.getCurrentPosition(),
+                          robot.leftBackMotor.getCurrentPosition(),
+                          robot.rightBackMotor.getCurrentPosition());
+        telemetry.update();
+
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+        telemetry.addData("Blue", robot.colorSensor.blue());
+        telemetry.addData("Red", robot.colorSensor.red());
+
+
+        armDown(2.5);//THIS WILL MOVE THE ARM WITH THE COLOR SENSOR DOWN
+        jewel(2.5);//THIS WILL SCAN THE COLOR, DECIDE IN WHAT DIRECTION TO TURN, AND TURN
+
+        SafeZone(2.5, vuMark);
+
+
+
+
+        sleep(1000);     // pause for servos to move
+
+        telemetry.addData("Path", "Complete");
+        telemetry.update();
+
+
+
+
+
     }
 
     /*
