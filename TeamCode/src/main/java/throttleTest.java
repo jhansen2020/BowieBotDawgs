aa13c576ef8b0b1eb674001f77710b96921fbc73
Nicolas Bastos <nickbastos4@gmail.com>
2018-09-12 15:37:26 -0500

    I created and wrote a new file to test having a throttle to set the power of the motors.

--- TeamCode/src/main/java/throttleTest.java
+++ TeamCode/src/main/java/throttleTest.java
@@ -14,7 +14,6 @@ import static android.os.SystemClock.sleep;
 public class throttleTest extends OpMode{
 
     //Creating variables
-
     private ElapsedTime runtime = new ElapsedTime();
 
     double leftJoyStick, rightJoyStick, leftMotorPower, rightMotorPower, liftMotorPower, clampMotorPower, leftServoPower, rightServoPower;
@@ -22,13 +21,13 @@ public class throttleTest extends OpMode{
     final private static double JOYSTICK_DEADBAND = 0.1;
 
     //Encoder Ticks Variables
-    static final double     COUNTS_PER_MOTOR_REV    = 1440 ;    // eg: TETRIX Motor Encoder
-    static final double     DRIVE_GEAR_REDUCTION    = 2.0 / 3 ;     // This is < 1.0 if geared UP
-    static final double     WHEEL_DIAMETER_INCHES   = 4.0 ;     // For figuring circumference
-    static final double     COUNTS_PER_INCH         = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
+    private static final double     COUNTS_PER_MOTOR_REV    = 1440 ;    // eg: TETRIX Motor Encoder
+    private static final double     DRIVE_GEAR_REDUCTION    = 2.0 / 3 ;     // This is < 1.0 if geared UP
+    private static final double     WHEEL_DIAMETER_INCHES   = 4.0 ;     // For figuring circumference
+    private static final double     COUNTS_PER_INCH         = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
             (WHEEL_DIAMETER_INCHES * 3.1415);
 
-    BotDawg robot;
+    private BotDawg robot;
 
     @Override
     public void init() {
@@ -57,30 +56,29 @@ public class throttleTest extends OpMode{
         }
 
         // assign throttle to the left and right motors
-        double right = throttle;
-        double left = throttle;
-
+        double rightThrottle = throttle;
+        double leftThrottle = throttle;
+        //I MIGHT NEED TO ADD JOYSTICK_DEADBAND
         // now we need to apply steering (direction). The left stick ranges from -1 to 1. If it is
         // negative we want to slow down the left motor. If it is positive we want to slow down the
         // right motor.
         if (gamepad2.left_stick_x < 0) {
             // negative value, stick is pulled to the left
-            left = left * (1 + gamepad2.left_stick_x);
+            leftThrottle = leftThrottle * (1 + gamepad2.left_stick_x);
         }
         if (gamepad2.left_stick_x > 0) {
             // positive value, stick is pulled to the right
-            right = right * (1 - gamepad2.left_stick_x);
+            rightThrottle = rightThrottle * (1 - gamepad2.left_stick_x);
         }
-
         // write the values to the motor. This will over write any values placed while processing gamepad1
-        robot.leftBackMotor.setPower(left);
-        robot.leftFrontMotor.setPower(left);
-        robot.rightFrontMotor.setPower(right);
-        robot.rightBackMotor.setPower(right);
+        robot.leftBackMotor.setPower(leftThrottle);
+        robot.leftFrontMotor.setPower(leftThrottle);
+        robot.rightFrontMotor.setPower(rightThrottle);
+        robot.rightBackMotor.setPower(rightThrottle);
 
         //Telemetry is not used to control the robot, it is purely to help debug by showing
         //Information on the phone
         telemetry.addData("Status", "Run Time: " + runtime.toString());
-        telemetry.addData("Motors", "left (%.2f), right (%.2f)", left, right);
+        telemetry.addData("Motors", "left (%.2f), right (%.2f)", leftThrottle, rightThrottle);
     }
 }
